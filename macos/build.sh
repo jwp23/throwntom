7f928d2661e817a477b653071f4fd07c37bf61b7
Joseph Presley <36678465+jwp23@users.noreply.github.com>
2026-08-28 08:04:07 -0700

    feat: wire app icon into Info.plist and build.sh

--- macos/build.sh
+++ macos/build.sh
@@ -17,9 +17,10 @@ swift build -c release --package-path "$PKG"
 BIN_DIR="$(swift build -c release --package-path "$PKG" --show-bin-path)"
 
 rm -rf "$APP"
-mkdir -p "$APP/Contents/MacOS" "$APP/Contents/Library/LaunchAgents"
+mkdir -p "$APP/Contents/MacOS" "$APP/Contents/Library/LaunchAgents" "$APP/Contents/Resources"
 sed "s/__VERSION__/$VERSION/g; s/__BUILD__/$BUILD/g" "$ROOT/macos/bundle/Info.plist" > "$APP/Contents/Info.plist"
 cp "$ROOT/macos/bundle/$LABEL.plist" "$APP/Contents/Library/LaunchAgents/"
+cp "$ROOT/macos/bundle/icon/Throwntom.icns" "$APP/Contents/Resources/Throwntom.icns"
 cp "$BIN_DIR/Throwntom" "$APP/Contents/MacOS/Throwntom"
 cp "$DEST/throwntomd" "$APP/Contents/MacOS/throwntomd"
 
