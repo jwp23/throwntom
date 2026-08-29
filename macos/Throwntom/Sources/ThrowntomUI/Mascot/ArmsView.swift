8c984235a2a1914553a844558328965e14768aa6
Joseph Presley <36678465+jwp23@users.noreply.github.com>
2026-08-29 12:06:06 -0700

    feat: animatable mascot arms

---
+++ macos/Throwntom/Sources/ThrowntomUI/Mascot/ArmsView.swift
@@ -0,0 +1,100 @@
+import SwiftUI
+
+// MARK: - Arm
+
+/// One arm: a cubic curve from the shoulder to the hand, in design units.
+struct Arm: Equatable {
+  init(_ sx: Double, _ sy: Double, _ c1x: Double, _ c1y: Double, _ c2x: Double, _ c2y: Double, _ hx: Double, _ hy: Double) {
+    shoulder = CGPoint(sx, sy)
+    control1 = CGPoint(c1x, c1y)
+    control2 = CGPoint(c2x, c2y)
+    hand = CGPoint(hx, hy)
+  }
+
+  var shoulder: CGPoint
+  var control1: CGPoint
+  var control2: CGPoint
+  var hand: CGPoint
+}
+
+// MARK: - ArmShape
+
+/// The arm's stroke path. Every point is animatable, so a pose change bends the arm from one
+/// curve into the next rather than cutting.
+struct ArmShape: Shape {
+  var arm: Arm
+
+  var animatableData: AnimatablePair<
+    AnimatablePair<CGPoint.AnimatableData, CGPoint.AnimatableData>,
+    AnimatablePair<CGPoint.AnimatableData, CGPoint.AnimatableData>,
+  > {
+    get {
+      AnimatablePair(
+        AnimatablePair(arm.shoulder.animatableData, arm.control1.animatableData),
+        AnimatablePair(arm.control2.animatableData, arm.hand.animatableData),
+      )
+    }
+    set {
+      arm.shoulder.animatableData = newValue.first.first
+      arm.control1.animatableData = newValue.first.second
+      arm.control2.animatableData = newValue.second.first
+      arm.hand.animatableData = newValue.second.second
+    }
+  }
+
+  func path(in rect: CGRect) -> Path {
+    let units = Units(rect: rect)
+    var path = Path()
+    path.move(to: units.point(arm.shoulder.x, arm.shoulder.y))
+    path.addCurve(
+      to: units.point(arm.hand.x, arm.hand.y),
+      control1: units.point(arm.control1.x, arm.control1.y),
+      control2: units.point(arm.control2.x, arm.control2.y),
+    )
+    return path
+  }
+}
+
+// MARK: - HandShape
+
+struct HandShape: Shape {
+  static let radius: Double = 4
+
+  var centre: CGPoint
+
+  var animatableData: CGPoint.AnimatableData {
+    get { centre.animatableData }
+    set { centre.animatableData = newValue }
+  }
+
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+    path.circle(Units(rect: rect), centre.x, centre.y, Self.radius)
+    return path
+  }
+}
+
+// MARK: - ArmsView
+
+/// Both arms as outline strokes with a body-coloured disc for each hand.
+struct ArmsView: View {
+  let left: Arm
+  let right: Arm
+  let unit: CGFloat
+
+  var body: some View {
+    ZStack {
+      arm(left)
+      arm(right)
+    }
+    .frame(width: Units.canvas * unit, height: Units.canvas * unit)
+  }
+
+  private func arm(_ arm: Arm) -> some View {
+    ZStack {
+      ArmShape(arm: arm).stroke(Palette.outline.color, style: StrokeStyle(lineWidth: 3 * unit, lineCap: .round))
+      HandShape(centre: arm.hand).fill(MascotPalette.body.color)
+      HandShape(centre: arm.hand).stroke(Palette.outline.color, lineWidth: 2 * unit)
+    }
+  }
+}
